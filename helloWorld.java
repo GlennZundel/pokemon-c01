e6ace03bef3e4bc58e89fcb84005ab2c812a1f88
BuildTools <unconfigured@null.spigotmc.org>
2023-10-26 20:48:08 +0200

    added helloworld

---
+++ helloWorld.java
@@ -0,0 +1,13 @@
+public class helloWorld {
+    
+    private static int test = 5;
+
+    public static int getTest(){
+        return test;
+    }
+
+    public static void main(String args[]){
+        System.out.println("Hello World");
+        System.out.println(getTest());
+    }
+}
